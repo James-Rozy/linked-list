4c5996731a4dfb198f2017cb87a3a34566adfa86
James Rozsypal <jimmerxl@gmail.com>
2022-08-24 11:06:28 -0400

    Add base classes and functions to linked-list.js

---
+++ linked-list.js
@@ -0,0 +1,25 @@
+class LinkedList {
+  append() {}
+
+  prepend() {}
+
+  size() {}
+
+  head() {}
+
+  tail() {}
+
+  at(index) {}
+
+  pop() {}
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {}
+}
+
+class Node {
+  constructor(value = null, nextNode = null) {}
+}
